e395ba7e16158e79694dc1cead57494fff947554
xiaolei.wang <123456>
2020-06-01 22:22:14 +0800

    log4j2

--- src/main/java/com/example/test/config/QuartzConfig.java
+++ src/main/java/com/example/test/config/QuartzConfig.java
@@ -1,10 +1,15 @@
 package com.example.test.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
+import javax.annotation.PostConstruct;
+
+@Slf4j
 @Configuration
 public class QuartzConfig {
     @Bean
@@ -17,5 +22,14 @@ public class QuartzConfig {
     @Bean(name = "scheduler")
     public Scheduler scheduler(){
         return schedulerFactoryBean().getScheduler();
+    }
+
+    @PostConstruct
+    public void startScheduler(){
+        try {
+            scheduler().start();
+        } catch (SchedulerException e) {
+            log.error(e.getMessage(), e);
+        }
     }
 }
